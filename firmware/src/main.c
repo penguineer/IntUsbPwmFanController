3ca75a3b3c08f6d0f036884135cd19f986f31c1c
Stefan Haun <mail@tuxathome.de>
2025-04-20 15:46:47 +0200

    Add a draft implementation of tacho measurement

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -25,7 +25,7 @@
 /***** Global variables *****/
 
 uint8_t pwm_target = 0; // Target PWM value
-uint16_t tacho_measurement = 0; // Current tacho measurement
+uint16_t tacho_rpm = 0; // Most recent tacho measurement
 
 /***** PWM *****/
 
@@ -58,6 +58,92 @@ void update_target_pwm(const uint8_t duty_cycle_param) {
     TCA0.SINGLE.CMP1 = temp >> 8;
 }
 
+/***** Tacho *****/
+
+void TCB_enable_event_routing(void) {
+    // Configure Event System to route PA6 to TCB0
+    EVSYS.ASYNCCH0 = EVSYS_ASYNCCH0_PORTA_PIN6_gc; // Pin PA6
+    EVSYS.ASYNCUSER0 = EVSYS_ASYNCUSER0_ASYNCCH0_gc; // route to TCB0
+}
+
+void TCB_disable_event_routing(void) {
+    // Disable the channel
+    EVSYS.ASYNCCH0 = EVSYS_ASYNCCH0_OFF_gc;
+}
+
+void TCB_init_tacho_counter(void) {
+    // Configure PA6 as input
+    PORTA.DIRCLR = PIN6_bm; // Set PA6 as input
+    PORTA.PIN6CTRL = PORT_ISC_RISING_gc; // Enable sensing on rising edge
+
+    TCB_enable_event_routing();
+
+    // Configure TCB0 in event counting mode
+    TCB0.CTRLA = TCB_CLKSEL_CLKDIV1_gc | TCB_ENABLE_bm; // Enable TCB0 with no prescaler
+    TCB0.CTRLB = TCB_CNTMODE_FRQ_gc; // Frequency measurement mode (event counting)
+
+    // Clear the counter
+    TCB0.CNT = 0;
+}
+
+uint16_t TCB_get_and_reset_count(void) {
+    // Disable the event generator to prevent race conditions
+    TCB_disable_event_routing();
+
+    // Read the current count
+    const uint16_t count = TCB0.CNT;
+    // Clear the counter
+    TCB0.CNT = 0;
+
+    // Enable event generator again
+    TCB_enable_event_routing();
+
+    // Return the current counter value
+    return count;
+}
+
+void RTC_init(void) {
+    // Wait until RTC is not busy
+    while (RTC.STATUS > 0)
+        /* busy wait */;
+
+    // Initialize RTC function
+    // (ATtiny 202 Datasheet 22.3)
+
+    // Configure RTC clock source to use the 1 kHz internal oscillator
+    RTC.CLKSEL = RTC_CLKSEL_INT1K_gc;
+
+    // Set the period to 1000 (1 second with 1 kHz clock)
+    RTC.PER = 1000;
+
+    // Enable the overflow (OVF) interrupt
+    RTC.INTCTRL = RTC_OVF_bm;
+
+    // Configure the prescaler to divide by 1 and enable the RTC
+    RTC.CTRLA = RTC_PRESCALER_DIV1_gc | RTC_RTCEN_bm;
+
+    // Clear any existing interrupt flags
+    RTC.INTFLAGS = RTC_OVF_bm;
+}
+
+ISR(RTC_CNT_vect) {
+    // We are reading the TCB0 counter once per second
+    const uint16_t revolutions_per_second = TCB_get_and_reset_count();
+
+    // Convert to RPM according to specification
+    // See https://noctua.at/pub/media/wysiwyg/Noctua_PWM_specifications_white_paper.pdf for details.
+    // There are two pulses per revolution.
+    const uint16_t rpm = revolutions_per_second * 60 >> 1;
+
+    // Atomic store
+    cli();
+    tacho_rpm = rpm;
+    sei();
+
+    // Clear the overflow interrupt flag
+    RTC.INTFLAGS = RTC_OVF_bm;
+}
+
 /***** TWI Core *****/
 
 void TWI_slave_init(void) {
@@ -85,11 +171,13 @@ static uint8_t tx_index = 0; // Current index in the buffer
 static uint8_t tx_length = 0; // Number of bytes to send
 
 void TWI_prepare_response(const uint8_t *data, const uint8_t length) {
-    // Copy data to the transmission buffer
-    tx_length = (length > TWI_BUFFER_SIZE) ? TWI_BUFFER_SIZE : length;
-    // This loop is likely to be unrolled by the compiler
-    for (uint8_t i = 0; i < tx_length; i++) {
-        tx_buffer[i] = data[i];
+    if (data != NULL) {
+        // Copy data to the transmission buffer
+        tx_length = (length > TWI_BUFFER_SIZE) ? TWI_BUFFER_SIZE : length;
+        // This loop is likely to be unrolled by the compiler
+        for (uint8_t i = 0; i < tx_length; i++) {
+            tx_buffer[i] = data[i];
+        }
     }
 
     // Reset index for new transmission
@@ -171,7 +259,11 @@ void TWI_handle_master_command(const uint8_t command, const uint8_t *data, const
             }
             break;
         case TWI_CMD_GET_TACHO:
-            TWI_prepare_response((const uint8_t *) &tacho_measurement, sizeof(tacho_measurement));
+            // Atomic access
+            cli();
+            const uint16_t rpm = tacho_rpm;
+            sei();
+            TWI_prepare_response((const uint8_t *) &rpm, sizeof(rpm));
             break;
         default:
             // no data
@@ -193,6 +285,8 @@ void set_clock_to_20MHz(void) {
 int main(void) {
     set_clock_to_20MHz();
     TWI_slave_init();
+    TCB_init_tacho_counter();
+    RTC_init();
 
     // ReSharper disable once CppDFAEndlessLoop
     for (;;);
